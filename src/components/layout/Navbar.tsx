99639e29c087866a6400548d8fd26c62f3c4e8ed
Roman Koshchei <romankoshchei@gmail.com>
2022-10-16 21:28:03 +0300

    rel = noopener

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -45,7 +45,7 @@ const Navbar = ({ dark }: NavbarProps) => {
 
       {/* <Navlink href='/guides'>Guides</Navlink> */}
 
-      <a href='https://github.com/paragoda/leta/' target='_blank'>
+      <a href='https://github.com/paragoda/leta/' target='_blank' rel='noopener'>
         <TextButton>GitHub</TextButton>
       </a>
 
