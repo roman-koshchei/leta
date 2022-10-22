f262471618b9f137206aa69019eb3e2ce18ebe33
Roman Koshchei <romankoshchei@gmail.com>
2022-10-22 11:45:27 +0300

    remove product hunt button because many problems

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -12,7 +12,6 @@ const AccountPage: NextPage = () => {
     <Center>
       <div className='flex flex-col gap-5'>
         <h2>Hi! Support us on product hunt</h2>
-        <a href="https://www.producthunt.com/posts/leta?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-leta" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=363667&theme=neutral" alt="LETA - You&#0032;can&#0032;type&#0032;faster&#0032;with&#0032;alternative&#0032;keyboard&#0032;layouts | Product Hunt" width="250" height="54" /></a>
         <OutlinedButton onClick={signOut}>Sign out</OutlinedButton>
       </div>
     </Center>
